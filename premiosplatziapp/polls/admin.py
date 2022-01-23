5129c01a05b3b74b768ccd87ec0bcb63f10f81ee
David Gerónimo Quiroga Torres <gedaquito@gmail.com>
2022-01-22 20:50:05 -0500

    Se crea el tempalte paa el index y se hacen las vistas del MTC

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+class ChoicesInline(admin.StackedInline):
+    model = Choice
+    can_delete = False
+    verbose_name_plural = 'choices'
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = (ChoicesInline,)
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 #Respuesta HTTP
 from django.http import HttpResponse
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-    # return render(request, 'polls/index.html')
+    # latest_question_list = Question.objects.filter('-pub_date')[:5]
+    latest_question_list = Question.objects.all()
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
